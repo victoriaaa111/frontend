d4b728d694646f123cd4dc2b3ac5dd3c945eeede
Andrei CEAETCHII <ceaetchiiandrei@gmail.com>
2024-09-20 14:01:38 +0300

    scrumm66: resolve the bug 401

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,73 @@
 import axios from 'axios';
 
+const adminApi = axios.create({
+    baseURL: 'http://3.70.72.246:3001/admin/login'
+});
+
+
+const clientApi = axios.create({
+    baseURL: 'http://3.70.72.246:3001/auth/login'
+});
+
+const clientSignUpApi = axios.create({
+    baseURL: 'http://3.70.72.246:3001/auth/signup'
+});
+
+// const workerSignUpApi = (workerId) => {
+//     return axios.create({
+//         baseURL: `http://3.70.72.246:3001/worker/${workerId}`
+//     });
+// } 
+
+const workerSignUpApi = axios.create({
+    baseURL: `http://3.70.72.246:3001/auth/worker/signup`
+});
+
+const workerSignInApi = axios.create({
+    baseURL:'http://3.70.72.246:3001/auth/worker/login'
+})
+
+const workerPrfileAPI = axios.create({
+    baseURL: 'http://3.70.72.246:3001/worker/edit/:workerid'
+});
+
+
+const workerGetDataApi = (workerId) =>{
+    return axios.get(`http://3.70.72.246:3001/worker/${workerId}`);
+}
+
+const workerPutUpdateDataApi = (workerId) =>{
+    return axios.create({
+        baseURL: `http://3.70.72.246:3001/worker/edit/${workerId}`
+    });
+}
+
+const serviceApi = (workerId) =>{
+    return axios.create({
+        baseURL: `http://3.70.72.246:3001/worker/add/${workerId}`
+    });
+}
+const Rating = (workerId) => {
+    return axios.create({
+        baseURL: `http://3.70.72.246:3001/worker/${workerId}`
+    })
+}
+
+const deleteService = (workerId, serviceId) => {
+    return axios.create({
+        baseURL: `http://3.70.72.246:3001/worker/${workerId}/service/${serviceId}`
+    })
+}
+
+const searchAPI = axios.create({
+    baseURL: 'http://3.70.72.246:3001/shareable/search',})
+
+
+
+const workerGetOrdersApi = axios.create({
+    baseURL: 'http://3.70.72.246:3001',
+})
+
 axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(request => {
@@ -43,78 +111,5 @@ axios.interceptors.response.use(
         return Promise.reject(error); // For all other errors, return the error as is.
     }
 );
-
-
-
-const adminApi = axios.create({
-    baseURL: 'http://3.70.72.246:3001/admin/login'
-});
-
-
-const clientApi = axios.create({
-    baseURL: 'http://3.70.72.246:3001/auth/login'
-});
-
-const clientSignUpApi = axios.create({
-    baseURL: 'http://3.70.72.246:3001/auth/signup'
-});
-
-// const workerSignUpApi = (workerId) => {
-//     return axios.create({
-//         baseURL: `http://3.70.72.246:3001/worker/${workerId}`
-//     });
-// } 
-
-const workerSignUpApi = axios.create({
-    baseURL: `http://3.70.72.246:3001/auth/worker/signup`
-});
-
-const workerSignInApi = axios.create({
-    baseURL:'http://3.70.72.246:3001/auth/worker/login'
-})
-
-const workerPrfileAPI = axios.create({
-    baseURL: 'http://3.70.72.246:3001/worker/edit/:workerid'
-});
-
-
-const workerGetDataApi = (workerId) =>{
-    return axios.create({
-        baseURL: `http://3.70.72.246:3001/worker/${workerId}`
-    });
-}
-
-const workerPutUpdateDataApi = (workerId) =>{
-    return axios.create({
-        baseURL: `http://3.70.72.246:3001/worker/edit/${workerId}`
-    });
-}
-
-const serviceApi = (workerId) =>{
-    return axios.create({
-        baseURL: `http://3.70.72.246:3001/worker/add/${workerId}`
-    });
-}
-const Rating = (workerId) => {
-    return axios.create({
-        baseURL: `http://3.70.72.246:3001/worker/${workerId}`
-    })
-}
-
-const deleteService = (workerId, serviceId) => {
-    return axios.create({
-        baseURL: `http://3.70.72.246:3001/worker/${workerId}/service/${serviceId}`
-    })
-}
-
-const searchAPI = axios.create({
-    baseURL: 'http://3.70.72.246:3001/shareable/search',})
-
-
-
-const workerGetOrdersApi = axios.create({
-    baseURL: 'http://3.70.72.246:3001',
-})
-
 
 export { adminApi, clientApi, clientSignUpApi, workerSignUpApi, workerPrfileAPI, workerSignInApi, workerGetDataApi, workerPutUpdateDataApi,serviceApi,Rating,deleteService, searchAPI, workerGetOrdersApi};
